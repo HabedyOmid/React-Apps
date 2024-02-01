75d854c75e98b1b89666a532ab766f95a91e8af6
HabedyOmid <omidiskawous@gmail.com>
2024-01-31 19:13:06 -0600

    challange 9 completed

--- challanges/src/CurrencyConverter.js
+++ challanges/src/CurrencyConverter.js
@@ -5,51 +5,77 @@ export default function CurrencyConverter() {
   const [to, setTo] = useState('USD');
   const [from, setFrom] = useState('EUR');
   const [result, setResult] = useState('');
+  const [isLoading, setIsLoading] = useState('');
+  const [error, setError] = useState('');
 
   useEffect(
     function () {
       const fetchRate = async () => {
-        if (amount <= 0) return;
-        const res = await fetch(
-          `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`
-        );
+        try {
+          setIsLoading(true);
 
-        const data = await res.json();
+          const res = await fetch(
+            `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`
+          );
 
-        setResult(data.rates[to]);
+          const data = await res.json();
+          setResult(data.rates[to]);
+        } catch (error) {
+          setError(error.message);
+        } finally {
+          setIsLoading(false);
+        }
       };
 
-      fetchRate();
+      if (amount !== 0 || from !== to) {
+        fetchRate();
+      }
     },
     [amount, from, to]
   );
 
   return (
     <div>
-      <p>
-        {amount} {from} to {to} = <strong>{result.USD}</strong>
-      </p>
+      {isLoading && !error && <p>Loading</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && (
+        <>
+          <p>
+            {amount} {from} to {to} = <strong>{result.USD}</strong>
+          </p>
+          <p>
+            {result} {to}
+          </p>
+        </>
+      )}
+
       <input
         value={amount}
         min="1"
         type="number"
+        disabled={isLoading}
         onChange={(e) => setAmount(Number(e.target.value))}
       />
-      <select value={from} onChange={(e) => setFrom(e.target.value)}>
+      <select
+        value={from}
+        onChange={(e) => setFrom(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select value={to} onChange={(e) => setTo(e.target.value)}>
+      <select
+        value={to}
+        onChange={(e) => setTo(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>
-        {result} {to}
-      </p>
     </div>
   );
 }
