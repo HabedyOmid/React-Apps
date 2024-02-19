3dd8a34f691957766fc99e7751d7bf3d7cac2e0c
HabedyOmid <omidiskawous@gmail.com>
2024-02-19 15:37:30 -0600

    react-router, loader and action wired up

--- fast-pizza/src/features/order/CreateOrder.jsx
+++ fast-pizza/src/features/order/CreateOrder.jsx
@@ -1,29 +1,26 @@
-import { useState } from "react";
-
-// https://uibakery.io/regex-library/phone-number
-const isValidPhone = (str) =>
-  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
-  );
+import { useState } from 'react';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
+import { isValidPhone } from '../../utils/helpers';
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -31,30 +28,42 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
-  // const [withPriority, setWithPriority] = useState(false);
+  const [withPriority, setWithPriority] = useState(false);
+
+  // use useNavigation() hook from react-router to access form action/submition state in this case
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
+  // use useActionData() hook from react-router to get the data return from action that is associcated with component
+  const formErrors = useActionData();
+
   const cart = fakeCart;
 
   return (
     <div>
       <h2>Ready to order? Let's go!</h2>
 
-      <form>
+      {/* magic react-router use this '<Form method="POST"></Form>' to capture
+      form data automatically and pass them to action() so later can be used to send to API */}
+      <Form method="POST">
         <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required />
+          <label htmlFor="name">First Name</label>
+          <input id="name" type="text" name="customer" required />
         </div>
 
         <div>
-          <label>Phone number</label>
+          <label htmlFor="phone">Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input id="phone" type="tel" name="phone" required />
           </div>
+
+          {formErrors?.phone && <p>{formErrors?.phone}</p>}
         </div>
 
         <div>
-          <label>Address</label>
+          <label htmlFor="address">Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input id="address" type="text" name="address" required />
           </div>
         </div>
 
@@ -63,18 +72,50 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
         <div>
-          <button>Order now</button>
+          <input name="cart" value={JSON.stringify(cart)} type="hidden" />
+          <button disabled={isSubmitting}>
+            {isSubmitting ? 'Creating order...' : 'Order now'}
+          </button>
         </div>
-      </form>
+      </Form>
     </div>
   );
+}
+
+// actions are used to mutate data or send data to API using react-router
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === 'on',
+  };
+
+  // Error handling before submitting to API
+  // construct errors object, pass component
+  const errors = {};
+
+  if (!isValidPhone(order.phone))
+    errors.phone = 'Please provide a valid phone number!';
+
+  // pass errors{} if there is any, component can access this data later
+  if (Object.keys(errors).length > 0) return errors;
+
+  // NO error create the order and redirect
+  const createdOrder = await createOrder(order);
+
+  // react-route provides redirect() since the useNavigate is
+  // not allowed since its not a component rather a action function
+  return redirect(`/order/${createdOrder.id}`);
 }
 
 export default CreateOrder;
--- fast-pizza/src/ui/Error.jsx
+++ fast-pizza/src/ui/Error.jsx
@@ -1,15 +1,20 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useRouteError } from 'react-router-dom';
 
-function NotFound() {
+function Error() {
   const navigate = useNavigate();
+
+  // This component has acccess to error 'useRouteError' from react-router
+  // The error object is global app error, 404 page, custom errors set inside
+  // components, loader() or any async functions
+  const error = useRouteError();
 
   return (
     <div>
       <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
+      <p>{error.data || error.message}</p>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
 }
 
-export default NotFound;
+export default Error;
